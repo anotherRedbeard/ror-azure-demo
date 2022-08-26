cb6e0418d9979151d21a49e05b200d3d0a775679
anotherRedbeard <andrewredman@redbeard-mbp.lan>
2022-08-26 09:00:42 -0500

    add twitter tweet

--- app/assets/javascripts/twitter.feed.js
+++ app/assets/javascripts/twitter.feed.js
@@ -1,1 +1,13 @@
 // TODO: [@marc_gagne] Implement a twitter client ;)
+twttr.widgets.createTweet(
+    '20',
+    document.getElementById('container'),
+    {
+      theme: 'dark'
+    }
+  );
+
+  twttr.widgets.createTweet(...)
+  .then( function( el ) {
+    console.log('Tweet added.');
+  });
